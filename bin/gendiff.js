1b607a8a67e04b33705cd9d5a96817aedfcd27b0
n8creator <n8creator@pm.me>
2023-03-14 13:05:08 +0300

    Step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
 
 const program = new Command();
@@ -7,6 +6,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two config files and shows difference')
-  .version('0.1.1', '-V, --version', 'output the version number');
+  .version('0.1.1', '-V, --version', 'output the version number')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
